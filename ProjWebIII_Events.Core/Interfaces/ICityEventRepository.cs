7103e72b6d77e805d936e11e37761669e9507687
Marcus Fabio <fabio63399@gmail.com>
2022-09-17 17:41:53 -0300

    Inclusão da interface ICityEventRepository

---
+++ ProjWebIII_Events.Core/Interfaces/ICityEventRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProjWebIII_Events.Core.Models;
+
+namespace ProjWebIII_Events.Core.Interfaces
+{
+    public interface ICityEventRepository
+    {
+        List<CityEvent> GetAllEventsRep();
+        public CityEvent GetEventByIdRep(long IdEvent);
+        public List<CityEvent> GetCityEventsByWordRep(string Title);
+        public List<CityEvent> GetCityEventsByLocalAndDateRep(string local, DateTime dateHourEvent);
+        public List<CityEvent> GetCityEventsByPriceAndDateRep(decimal minPrice, decimal maxPrice, DateTime date);
+        public bool InsertNewEventRep(CityEvent cityEvent);
+        public bool UpdateEventRep(long IdEvent, CityEvent cityEvent);
+        public bool DeleteEventRep(long IdEvent);
+        public bool UpdateEventRep(long IdEvent);
+    }
+}
