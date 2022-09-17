3476cf7cec704d5a14af40e44bc2fba33877ab62
Marcus Fabio <fabio63399@gmail.com>
2022-09-17 17:41:34 -0300

    Program.cs

--- ProjWebIII_Events/Program.cs
+++ ProjWebIII_Events/Program.cs
@@ -1,11 +1,50 @@
+using ProjWebIII_Events.Core.Interfaces;
+using ProjWebIII_Events.Core.Services;
+using ProjWebIII_Events.Filters;
+using ProjWebIII_Events.Infra.Data.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration["secretKey"]);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) //Adiciono o esquema de JWT Bearer
+    .AddJwtBearer(options =>
+    {
+        //Adiciona as opções de validação
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true, // para inativar a validação do issuer, informar false e remover ValidIssuer
+            ValidateAudience = true, // para inativar a validação da audience, informar false e remover ValidAudience
+            ValidIssuer = "APIClientes.com",
+            ValidAudience = "APIEvents.com"
+        };
+    });
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(options =>
+{
+    options.Filters.Add<LogResultFilter>();
+    options.Filters.Add<GeneralExceptionFilter>();
+});
+
+
+builder.Services.AddScoped<ICityEventRepository, CityEventRepository>();
+builder.Services.AddScoped<ICityEventService, CityEventService>();
+builder.Services.AddScoped<IEventReservationService, EventReservationService>();
+builder.Services.AddScoped<IEventReservationRepository, EventReservationRepository>();
+builder.Services.AddScoped<EventExistsById>();
 
 var app = builder.Build();
 
@@ -18,6 +57,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
