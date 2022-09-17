c0a474b0bb71d7b20d5e6aa21e15130510ae8529
Marcus Fabio <fabio63399@gmail.com>
2022-09-17 17:42:52 -0300

    Inclusão da interface IEventReservationService

---
+++ ProjWebIII_Events.Core/Interfaces/IEventReservationService.cs
@@ -0,0 +1,16 @@
+﻿using ProjWebIII_Events.Core.Models;
+
+namespace ProjWebIII_Events.Core.Interfaces
+{
+    public interface IEventReservationService
+    {
+        List<EventReservation> GetAllReservations();
+        List<EventReservation> GetReservationByName(string personName);
+        EventReservation GetReservationById(long IdReservation);
+        List<Object> GetReservationByNameAndTitle(string personName, string title);
+        bool InsertReservation(EventReservation eventReservation);
+        bool UpdateReservationQuantity(long IdReservation, long Quantity);
+        bool DeleteReservation(long IdEvent);
+        bool CheckReservation(long IdEvent);
+    }
+}
