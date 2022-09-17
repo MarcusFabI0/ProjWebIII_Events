fc0a708791889ee73877d281afb5b051bb9b692b
Marcus Fabio <fabio63399@gmail.com>
2022-09-17 17:42:14 -0300

    Inclusão da interface ICityEventService

---
+++ ProjWebIII_Events.Core/Interfaces/ICityEventService.cs
@@ -0,0 +1,18 @@
+﻿using ProjWebIII_Events.Core.Models;
+
+namespace ProjWebIII_Events.Core.Interfaces
+{
+    public interface ICityEventService
+    {
+        List<CityEvent> GetAllEvents();
+        public CityEvent GetEventById(long IdEvent);
+        List<CityEvent> GetCityEventsByWord(string Title);
+        public List<CityEvent> GetCityEventsByLocalAndDate(string local, DateTime dateHourEvent);
+        public List<CityEvent> GetCityEventsByPriceAndDate(decimal minPrice, decimal maxPrice, DateTime dateHourEvent);
+        public bool InsertNewEvent(CityEvent cityEvent);
+        public bool UpdateEvent(long IdEvent, CityEvent cityEvent);
+        public bool DeleteEvent(long IdEvent);
+
+       
+    }
+}
