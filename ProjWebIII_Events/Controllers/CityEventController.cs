5bd85b715cfa6cd3b2740ab4cd2d0b5d2d3e2429
Marcus Fabio <fabio63399@gmail.com>
2022-09-17 17:40:15 -0300

    Inclusão das controllers CityEventController e EventReservationController

---
+++ ProjWebIII_Events/Controllers/CityEventController.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjWebIII_Events.Core.Interfaces;
+using ProjWebIII_Events.Core.Models;
+using ProjWebIII_Events.Filters;
+
+namespace ProjWebIII_Events.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [TypeFilter(typeof(LogResourceFilter))]
+    
+    public class CityEventController : ControllerBase
+    {
+        public List<CityEvent> cityEventList = new()
+        {
+        };
+
+        public ICityEventService _cityEventService;
+        public CityEventController(ICityEventService cityEventService)
+        {
+            _cityEventService = cityEventService;
+        }
+
+        #region GetAllEvents
+        [HttpGet("/events/search/all_events")]//consulta todos o eventos.
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<List<CityEvent>> GetAllEvents()
+        {
+            var search = _cityEventService.GetAllEvents();
+
+            return Ok(search);
+        }
+        #endregion
+
+        #region GetEventById
+        [HttpGet("/events/search/event_{IdEvent}")]// consulta os eventos com base no ID informado.
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public ActionResult<CityEvent> GetEventbyId(long IdEvent)
+        {
+            var search = _cityEventService.GetEventById(IdEvent);
+
+            if (search == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(search);
+        }
+        #endregion
+
+        #region GetCityEventsByWord
+        [HttpGet("/events/search/{title}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<CityEvent>> GetCityEventsByWord(string Title)
+        {
+            var search = _cityEventService.GetCityEventsByWord(Title);
+
+            if (search == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(search);
+        }
+        #endregion
+
+        #region GetCityEventsByLocalAndDate
+        [HttpGet("/events/search/{local}_{dateHourEvent}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<CityEvent>> GetCityEventsByLocalAndDate(string local, DateTime dateHourEvent)
+        {
+            var search = _cityEventService.GetCityEventsByLocalAndDate(local, dateHourEvent);
+
+            if (search == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(search);
+        }
+        #endregion
+
+        #region GetCityEventsByPriceAndDate
+        [HttpGet("/events/search/{minPrice}_{maxPrice}_{dateHourEvent}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<CityEvent> GetCityEventsByPriceAndDate(decimal minPrice, decimal maxPrice, DateTime dateHourEvent)
+        {
+            var cityEvents = _cityEventService.GetCityEventsByPriceAndDate(minPrice, maxPrice, dateHourEvent);
+
+            if (cityEvents == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(cityEvents);
+        }
+        #endregion
+
+        #region InsertNewEvent
+        [HttpPost("/events/newevent")]//insere um novo evento
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [TypeFilter(typeof(LogActionFilter))]
+        [Authorize(Roles = ("admin"))]
+        public IActionResult InsertNewEvent(CityEvent cityEvent)
+        {
+            var newEvent = _cityEventService.InsertNewEvent(cityEvent);
+            if (!newEvent)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            return CreatedAtAction(nameof(InsertNewEvent), cityEvent);
+        }
+        #endregion
+
+        #region UpdateEvent
+        [HttpPut("/events/update")]//atualiza o evento
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ServiceFilter(typeof(EventExistsById))]
+        [Authorize(Roles = ("admin"))]
+        public IActionResult UpdateEvent(long IdEvent, CityEvent cityEvent)
+        {
+            if (!_cityEventService.UpdateEvent(IdEvent, cityEvent))
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return NoContent();
+
+        }
+        #endregion
+
+        #region DeleteEvent
+        [HttpDelete("/events/delete")]//deleta o evento
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ServiceFilter(typeof(EventExistsById))]
+        [Authorize(Roles = ("admin"))]
+        public ActionResult<List<CityEvent>> DeleteEvent(long IdEvent)
+        {
+            var delete = _cityEventService.DeleteEvent(IdEvent);
+            if (!delete)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return Ok(delete);
+        }
+        #endregion
+
+    }
+}
+
+
+
---
+++ ProjWebIII_Events/Controllers/EventReservationController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjWebIII_Events.Core.Interfaces;
+using ProjWebIII_Events.Core.Models;
+using ProjWebIII_Events.Filters;
+
+namespace ProjWebIII_Events.Controllers
+{
+    [Route("api/[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ApiController]
+    public class EventReservationController : ControllerBase
+    {
+        public List<EventReservation> eventReservationList = new()
+        {
+        };
+
+        public IEventReservationService _eventReservationService;
+        public EventReservationController(IEventReservationService eventReservationService)
+        {
+            _eventReservationService = eventReservationService;
+        }
+
+        #region GetAllReservations
+        [HttpGet("/reservations/search/all_reservations")]//consulta todas as reservas.
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<EventReservation>> GetAllReservations()
+        {
+            var search = _eventReservationService.GetAllReservations();
+            return Ok(search);
+        }
+        #endregion
+
+        #region GetReservationByName
+        [HttpGet("/reservations/search/reservation_{personName}")]// consulta as reservas com base no nome informado.
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<EventReservation>> GetReservationByName(string personName)
+        {
+            var search = _eventReservationService.GetReservationByName(personName);
+            if (search == null)
+            {
+                return NotFound();
+            }
+            return Ok(search);
+        }
+        #endregion
+
+        #region GetReservationByNameAndTitle
+        [HttpGet("/reservations/search/reservation_{personName}_{title}")]
+        [Authorize("admin, cliente")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Roles = ("admin, cliente"))]
+        public ActionResult<List<Object>> GetReservationByNameAndTitle(string personName, string title)
+        {
+            var search = _eventReservationService.GetReservationByNameAndTitle(personName, title);
+            if (search == null)
+            {
+                return NotFound();
+            }
+            return Ok(search);
+        }
+        #endregion
+
+        #region GetReservationById
+        [HttpGet("/reservations/search/reservation_{IdReservation}")]// consulta as reservas com base no ID informado.
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ServiceFilter(typeof(EventExistsById))]
+        public ActionResult<EventReservation> GetReservationById(long IdReservation)
+        {
+            var search = _eventReservationService.GetReservationById(IdReservation);
+            if (search == null)
+            {
+                return NotFound();
+            }
+            return Ok(search);
+        }
+        #endregion
+
+        #region InsertReservation
+        [HttpPost("/reservations/newreservation")]//insere uma nova reserva.
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Authorize(Roles = ("admin, cliente"))]
+
+        public IActionResult InsertReservation(EventReservation eventReservation)
+        {
+            var newReservation = _eventReservationService.InsertReservation(eventReservation);
+            if (!newReservation)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            return CreatedAtAction(nameof(InsertReservation), eventReservation);
+        }
+        #endregion
+
+        #region UpdateReservationQuantity
+        [HttpPut("/reservations/update")]// atualiza a quantidade de reservas
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ServiceFilter(typeof(EventExistsById))]
+        [Authorize(Roles = ("admin"))]
+        public IActionResult UpdateReservationQuantity(long IdReservation, long Quantity)
+        {
+            var update = _eventReservationService.UpdateReservationQuantity(IdReservation, Quantity);
+            if (!update)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return Ok();
+
+        }
+        #endregion
+
+        #region DeleteReservation
+        [HttpDelete("/reservations/delete")]// deleta reserva com base no id informado.
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ServiceFilter(typeof(EventExistsById))]
+        [Authorize(Roles = ("admin"))]
+
+        public ActionResult<List<EventReservation>> DeleteReservation(long IdReservation)
+        {
+            if (!_eventReservationService.DeleteReservation(IdReservation))
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return Ok(eventReservationList);
+        }
+        #endregion
+    }
+}
+
