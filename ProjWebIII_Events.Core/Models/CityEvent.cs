e3bebbf01cb7eb4de699dd83aa6da6f0018b85a5
Marcus Fabio <fabio63399@gmail.com>
2022-09-17 17:43:17 -0300

    Inclusão das models CityEvent e EventReservation.

---
+++ ProjWebIII_Events.Core/Models/CityEvent.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjWebIII_Events.Core.Models
+{
+    public class CityEvent
+    {
+        public long IdEvent { get; set; }
+
+        [Required(ErrorMessage = "Título é obrigatório")]
+        [MaxLength(100, ErrorMessage = "Titulo deve ter no máximo 100 caracteres")]
+        public string Title { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Descrição deve ter no máximo 1000 caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Data é obrigatória")]
+        public DateTime DateHourEvent { get; set; }
+
+        [Required(ErrorMessage = "Local é obrigatório")]
+        [MaxLength(100, ErrorMessage = "Local deve ter no máximo 100 caracteres")]
+        public string Local { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Nome deve ter no máximo 100 caracteres")]
+        public string Address { get; set; }
+
+       
+        public decimal Price { get; set; }
+
+        public bool Status {get; set;}
+        
+
+            
+
+    }
+}
+
+
+//        IdEvent long incremento PK
+//Title                 string not null
+//Description string        null
+//DateHourEvent DateTime    not null
+//Local string not null
+//Address string        null
+//Price decimal        null
+//Status bit        not null
---
+++ ProjWebIII_Events.Core/Models/EventReservation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjWebIII_Events.Core.Models
+{
+    public class EventReservation
+    {
+        public long IdReservation { get; set; }
+
+        [Required(ErrorMessage = "ID é obrigatório")]
+        public long IdEvent { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Nome deve ter no máximo 100 caracteres")]
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string? PersonName { get; set; }
+
+        [Required(ErrorMessage = "Quantidade é obrigatória")]
+        public long Quantity { get; set; }
+    }
+}
+
+//EventReservation:
+//IdReservation        long incremento PK
+//IdEvent                long        not null FK
+//PersonName            string        not null
+//Quantity            long        not null
