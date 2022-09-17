11ea9dbf75569b1788909713e7df1373eeb30d93
Marcus Fabio <fabio63399@gmail.com>
2022-09-17 17:44:57 -0300

    Inclusão das services CityEventService e EventReservationService.

---
+++ ProjWebIII_Events.Core/Services/CityEventService.cs
@@ -0,0 +1,61 @@
+﻿using ProjWebIII_Events.Core.Interfaces;
+using ProjWebIII_Events.Core.Models;
+
+namespace ProjWebIII_Events.Core.Services
+{
+    public class CityEventService : ICityEventService
+    {
+
+        public ICityEventRepository _cityEventRepository;
+        public IEventReservationService _eventReservationService;
+        public CityEventService(ICityEventRepository cityEventRepository, IEventReservationService eventReservationService)
+        {
+            _cityEventRepository = cityEventRepository;
+            _eventReservationService = eventReservationService;
+        }
+
+        public List<CityEvent> GetAllEvents()
+        {
+            return _cityEventRepository.GetAllEventsRep();
+        }
+        public CityEvent GetEventById(long IdEvent)
+        {
+            return _cityEventRepository.GetEventByIdRep(IdEvent);
+        }
+
+        public List<CityEvent> GetCityEventsByWord(string Title)
+        {
+            return _cityEventRepository.GetCityEventsByWordRep(Title);
+        }
+
+        public List<CityEvent> GetCityEventsByLocalAndDate(string local, DateTime dateHourEvent)
+        {
+            return _cityEventRepository.GetCityEventsByLocalAndDateRep(local, dateHourEvent);
+        }
+        public List<CityEvent> GetCityEventsByPriceAndDate(decimal minPrice, decimal maxPrice, DateTime dateHourEvent)
+        {
+            return _cityEventRepository.GetCityEventsByPriceAndDateRep(minPrice, maxPrice, dateHourEvent);
+        }
+        public bool InsertNewEvent(CityEvent cityEvent)
+        {
+            return _cityEventRepository.InsertNewEventRep(cityEvent);
+        }
+
+        public bool UpdateEvent(long IdEvent, CityEvent cityEvent)
+        {
+            return _cityEventRepository.UpdateEventRep(IdEvent, cityEvent);
+        }
+
+        public bool DeleteEvent(long IdEvent)
+        {
+            if (_eventReservationService.CheckReservation(IdEvent))
+            {
+                return _cityEventRepository.UpdateEventRep(IdEvent);
+            }
+            return _cityEventRepository.DeleteEventRep(IdEvent);
+        }
+
+        
+    }
+}
+
---
+++ ProjWebIII_Events.Core/Services/EventReservationService.cs
@@ -0,0 +1,56 @@
+﻿using ProjWebIII_Events.Core.Interfaces;
+using ProjWebIII_Events.Core.Models;
+
+namespace ProjWebIII_Events.Core.Services
+{
+    public class EventReservationService : IEventReservationService
+    {
+        public IEventReservationRepository _eventReservationRepository;
+
+        public EventReservationService(IEventReservationRepository eventReservationRepository)
+
+        {
+            _eventReservationRepository = eventReservationRepository;
+        }
+
+        public List<EventReservation> GetAllReservations()
+        {
+            return _eventReservationRepository.GetAllReservationsRep();
+        }
+
+        public List<EventReservation> GetReservationByName(string personName)
+        {
+            return _eventReservationRepository.GetReservationByNameRep(personName);
+        }
+
+        public EventReservation GetReservationById(long IdReservation)
+        {
+            return _eventReservationRepository.GetReservationByIdRep(IdReservation);
+        }
+        public List<object> GetReservationByNameAndTitle(string personName, string title)
+        {
+            return _eventReservationRepository.GetReservationByNameAndTitleRep(personName, title);
+        }
+        public bool InsertReservation(EventReservation eventReservation)
+        {
+            return _eventReservationRepository.InsertReservationRep(eventReservation);
+        }
+
+        public bool UpdateReservationQuantity(long IdReservation, long Quantity)
+        {
+            return _eventReservationRepository.UpdateReservationRep(IdReservation, Quantity);
+        }
+
+        public bool DeleteReservation(long IdEvent)
+        {
+            return _eventReservationRepository.DeleteReservationEventRep(IdEvent);
+        }
+
+        public bool CheckReservation(long IdEvent)
+        {
+            return _eventReservationRepository.CheckReservationRep(IdEvent);
+        }
+
+        
+    }
+}
